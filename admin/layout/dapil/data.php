83ba3859c5344bc6668134ff360f04bb6a378d4b
azwarbahar07 <azwarbahar07@gmail.com>
2020-10-07 20:22:42 +0800

    Ubah data Dapil

--- admin/layout/dapil/data.php
+++ admin/layout/dapil/data.php
@@ -40,17 +40,79 @@ require '../../template/header.php';
                     <th>No</th>
                     <th>Nama</th>
                     <th>Daerah</th>
+                    <th>Anggota</th>
                     <th>Aksi</th>
                   </tr>
                   </thead>
                   <tbody>
 
                   <tr>
-                    <td>1</td>
-                    <td>3</td>
-                    <td>Takalalla - Liliriaja - Lilirilau</td>
+                    <td style="text-align:center">1</td>
+                    <td>Daerah Pilihan I</td>
+                    <td>
+
+                      <ul>
+                        <li>Kecamatan Marioriwawo</li>
+                      </ul>
+
+                    </td>
+                    <td><a href="#">9 anggota</a></td>
                     <td style="text-align:center">
-                        <a href="detail.php" type="button" class="btn btn-info"><i class="fa fa-eye"></i></a>
+                        <a href="edit.php" type="button" class="btn btn-secondary"><i class="fa fa-edit"></i></a>
+                        <a href="#" type="button" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td style="text-align:center">2</td>
+                    <td>Daerah Pilihan II</td>
+                    <td>
+                      <ul>
+                        <li>Kecamatan Lilirilau</li>
+                        <li>Kecamatan Liliriaja</li>
+                        <li>Kecamatan Citta</li>
+                      </ul>
+
+                    </td>
+                    <td><a href="#">3 anggota</a></td>
+                    <td style="text-align:center">
+                        <a href="edit.php" type="button" class="btn btn-secondary"><i class="fa fa-edit"></i></a>
+                        <a href="#" type="button" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td style="text-align:center">3</td>
+                    <td>Daerah Pilihan III</td>
+                    <td>
+                      <ul>
+                        <li>Kecamatan Lalabata</li>
+                        <li>Kecamatan Ganra</li>
+                      </ul>
+
+                    </td>
+                    <td><a href="#">3 anggota</a></td>
+                    <td style="text-align:center">
+                        <a href="edit.php" type="button" class="btn btn-secondary"><i class="fa fa-edit"></i></a>
+                        <a href="#" type="button" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+
+                    </td>
+                  </tr>
+                  
+                  <tr>
+                    <td style="text-align:center">4</td>
+                    <td>Daerah Pilihan IV</td>
+                    <td>
+                      <ul>
+                        <li>Kecamatan Marioriawa</li>
+                        <li>Kecamatan Donri Donri</li>
+                      </ul>
+
+                    </td>
+                    <td><a href="#">5 anggota</a></td>
+                    <td style="text-align:center">
                         <a href="edit.php" type="button" class="btn btn-secondary"><i class="fa fa-edit"></i></a>
                         <a href="#" type="button" class="btn btn-danger"><i class="fa fa-trash"></i></a>
 
