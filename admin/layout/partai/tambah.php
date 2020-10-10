cf1875140a6aa0f52e8cd8fd107d253bfb7b3e61
azwarbahar07 <azwarbahar07@gmail.com>
2020-10-10 20:07:26 +0800

    buat view tambah partai

--- admin/layout/partai/tambah.php
+++ admin/layout/partai/tambah.php
@@ -26,10 +26,50 @@ require '../../template/header.php';
 
 
 
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-10">
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Tambah Partai</h3>
+
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                  <i class="fas fa-minus"></i></button>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="form-group">
+                <label for="inputName">Nama Partai</label>
+                <input type="text" id="inputName" class="form-control">
+              </div>
+
+              <div class="form-group">
+                    <label for="customFile">Logo Partai</label>
+
+                    <div class="custom-file">
+                      <input type="file" class="custom-file-input" id="customFile">
+                      <label class="custom-file-label" for="customFile">Choose file</label>
+                    </div>
+                  </div>
+
+              <div class="col-12">
+              <a href="#" class="btn btn-success float-right" style="margin-top: 3% ; margin-left: 2%;">Simpan</a>
+              <a href="#" class="btn btn-secondary float-right" style="margin-top: 3% ;">Batal</a>
+            </div>
+            </div>
+            <!-- /.card-body -->
+
+            <br>
+          </div>
+          <!-- /.card -->
 
 
-
-    
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
     </div>
   <!-- /.content-wrapper -->
 
